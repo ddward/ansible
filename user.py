d6911b5c9e2d8011f65b00fd275f91de76587e52
Nik Szejna <szejna@gmail.com>
2021-01-22 18:08:10 -0600

    fix username case sensitivity
    Anytime a username is used to interact witht the database, it is formatted, currently this formatting just lowercases any username input

--- user.py
+++ user.py
@@ -5,21 +5,24 @@ import traceback
 
 def create_user(username,password):
     try:
+        formattedUsername = format_username(username)
         hashedPassword = generate_password_hash(password)
-        insert( 'user', ('username', 'password'), (username, hashedPassword))
+        insert( 'user', ('username', 'password'), (formattedUsername, hashedPassword))
     except Exception as e:
         logging.error(traceback.format_exc())
 
 def user_exists(username):
     try:
-        return exists('user','username',username)
+        formattedUsername = format_username(username)
+        return exists('user','username',formattedUsername)
     except Exception as e:
         logging.error(traceback.format_exc())
         print("User existence check failed")
 
 def get_user(username):
     try:
-        return select_one('user',('username','password'), 'username',username)
+        formattedUsername = format_username(username)
+        return select_one('user',('username','password'), 'username',formattedUsername)
     except Exception as e:
         logging.error(traceback.format_exc())
         print("Failed to get user")
@@ -27,12 +30,13 @@ def get_user(username):
 
 def update_user(username,password,new_password):
     try:
-        user = get_user(username)
+        formattedUsername = format_username(username)
+        user = get_user(formattedUsername)
         user_password = user[1]
         if(user is not None):
          if(check_password_hash(user_password,password)):
              newHashedPassword = generate_password_hash(new_password)
-             update('user',{'password':newHashedPassword},'username',username)
+             update('user',{'password':newHashedPassword},'username',formattedUsername)
     except:
         logging.error(traceback.format_exc())
 
@@ -53,3 +57,6 @@ def gen_default_user():
         create_user('default',password)
     except:
         logging.error(traceback.format_exc())
+
+def format_username(username):
+    return username.lower()
